8308302678755e2b0ec074976a7325d183cbe3f0
James Laver <james.laver@gmail.com>
2012-11-01 23:29:31 +0000

    Rename coffee-mode hook function

--- cust/elisp-coffee-mode.el
+++ cust/elisp-coffee-mode.el
@@ -1,10 +1,10 @@
 (require 'coffee-mode)
-(defun coffee-custom ()
+(defun elisp-coffee-custom ()
   "coffee-mode-hook"
   ; Indent with two spaces. Nodejs gets very nested
   (set (make-local-variable 'tab-width) 2)
   ; I haven't added js2-mode yet (or decided if i like it)
   (setq coffee-js-mode 'javascript-mode))
   
-(add-hook 'coffee-mode-hook '(lambda() (coffee-custom)))
+(add-hook 'coffee-mode-hook '(lambda() (elisp-coffee-custom)))
 (provide 'elisp-coffee-mode)
