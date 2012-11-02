c6e572889036b7742e3e1a247119631164253773
James Laver <james.laver@gmail.com>
2012-11-02 01:22:22 +0000

    Old comment from c/p removed

--- cust/elisp-php-mode.el
+++ cust/elisp-php-mode.el
@@ -1,7 +1,6 @@
 (require 'php-mode)
 (defun elisp-php-custom ()
   "php-mode-hook"
-  ; Indent with two spaces. Nodejs gets very nested
   (set (make-local-variable 'tab-width) 4))
   
 (add-hook 'php-mode-hook '(lambda() (elisp-php-custom)))
