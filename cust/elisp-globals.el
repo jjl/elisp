72ccc333c24ae845a1467fb7023d827abd307d50
James Laver <james.laver@gmail.com>
2012-11-13 18:12:31 +0000

    Turned tab indent off by default

--- cust/elisp-globals.el
+++ cust/elisp-globals.el
@@ -1,5 +1,8 @@
 ; Turn on syntax highlighting
 (global-font-lock-mode t)
+; Indentation
+(setq indent-tabs-mode nil)
+(setq tab-width 4)
 
 ;This fixes web mode for emacs23. And possibly fixes other modes too
 (unless (fboundp 'prog-mode) (defalias 'prog-mode 'fundamental-mode))
