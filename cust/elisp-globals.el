47c7608ae71039fa9657c0712b93f777d04bddce
James Laver <jjl@assahs>
2013-05-22 10:42:55 +0100

    Disabled bell where it's going to be a pain in the arse

--- cust/elisp-globals.el
+++ cust/elisp-globals.el
@@ -3,6 +3,8 @@
 ; Indentation
 (setq indent-tabs-mode nil)
 (setq tab-width 4)
+; Turn off the fucking bell
+(setq visible-bell 1)
 
 ;This fixes web mode for emacs23. And possibly fixes other modes too
 (unless (fboundp 'prog-mode) (defalias 'prog-mode 'fundamental-mode))
