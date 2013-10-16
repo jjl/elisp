22138df077c10dd11eb05efde1fcdd571539e556
James Laver <james@trustrobots.com>
2013-10-16 20:17:19 +0100

    Made scala-mode2 work properly

--- cust/scala-mode-cust.el
+++ cust/scala-mode-cust.el
@@ -1,6 +1,7 @@
 ; Auto selection
-(autoload 'scala-mode "scala-mode.el" "Scala Mode" t)
+(autoload 'scala-mode "scala-mode2.el" "Scala Mode" t)
 (add-to-list 'auto-mode-alist '("\\.scala\\'" . scala-mode))
+(add-to-list 'auto-mode-alist '("\\.sbt\\'" . scala-mode))
 
 ; Hook
 (defun scala-mode-custom ()
