2c7f7282d61171475331e6c4c674655fc7660d22
James Laver <james.laver@razorfish.com>
2013-11-05 18:19:59 +0000

    Subtree info and update script

---
+++ update-subtrees.sh
@@ -0,0 +1,11 @@
+git subtree pull --prefix vendor/buffer-timer https://github.com/emacsmirror/buffer-timer.git master --squash
+git subtree pull --prefix vendor/cc-mode-plus https://github.com/emacsmirror/cc-mode-plus.git master --squash
+git subtree pull --prefix vendor/chop https://github.com/emacsmirror/chop.git master --squash
+git subtree pull --prefix vendor/emacspeak https://github.com/emacsmirror/emacspeak.git master --squash
+git subtree pull --prefix vendor/eslide https://github.com/jrockway/eslide.git master --squash
+git subtree pull --prefix vendor/ff-paths https://github.com/emacsmirror/ff-paths.git master --squash
+git subtree pull --prefix vendor/hideshowvis https://github.com/emacsmirror/hideshowvis.git master --squash
+git subtree pull --prefix vendor/virtualenv https://github.com/aculich/virtualenv.el.git master --squash
+git subtree pull --prefix vendor/xs-mode https://github.com/emacsmirror/xs-mode.git master --squash
+git subtree pull --prefix vendor/xsl-mode https://github.com/emacsmirror/xsl-mode.git master --squash
+git subtree pull --prefix vendor/xterm-color https://github.com/emacsmirror/xterm-color.git master --squash
