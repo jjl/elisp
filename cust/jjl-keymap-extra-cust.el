bfbbebc0bb8ca6dfce343e3539489c28cb38b213
James Laver <james.laver@razorfish.com>
2013-11-20 17:15:13 +0000

    Bound mc/edit-lines to M-c l

--- cust/jjl-keymap-extra-cust.el
+++ cust/jjl-keymap-extra-cust.el
@@ -4,5 +4,8 @@
 ; Moving between errors
 (global-set-key (kbd "M-s e n") 'jjl-next-error)
 (global-set-key (kbd "M-s e p") 'jjl-previous-error)
-
+; Multiple cursors
+(global-unset-key (kbd "M-c"))
+(global-set-key (kbd "M-c l") 'mc/edit-lines)
+; Food for thought: stackoverflow.com/questions/7549259/is-there-a-quick-way-to-unbind-keys-in-emacs
 (provide 'jjl-keymap-extra-cust)
