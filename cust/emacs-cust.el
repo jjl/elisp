9db98d9aa417e15c5bfec580ae0548f40efb5197
James Laver <james@trustrobots.com>
2013-12-04 15:02:52 +0000

    change path, swap out yes-or-no-p for y-or-n-p

--- cust/emacs-cust.el
+++ cust/emacs-cust.el
@@ -6,5 +6,10 @@
 ; Remove irritations
 (setq visible-bell 1)
 (setq inhibit-startup-screen t)
+(setq yes-or-no-p 'y-or-n-p)
+; Fix PATH, which should fix flycheck to not use system perl etc.
+(setenv "PATH"
+	(concat "/usr/local/bin"
+		(getenv "PATH")))
 
 (provide 'emacs-cust)
