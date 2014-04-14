57a30fc600abaccf737d932522152787c8a406b7
James Laver <james@codegems.org>
2014-04-14 11:16:22 +0100

    C-M-: - uncomment-region

--- cust/jjl-keymap-cust.el
+++ cust/jjl-keymap-cust.el
@@ -3,5 +3,6 @@
 (global-unset-key (kbd "C-z")) ; iconify-frame
 (global-set-key (kbd "C-z") 'undo)
 (global-set-key (kbd "C-;") 'comment-region)
-(global-set-key (kbd "C-:") 'uncomment-region)
+;; Clojure-mode binds C-: to toggle between keyword and string, which is useful
+(global-set-key (kbd "C-M-:") 'uncomment-region)
 (provide 'jjl-keymap-cust)
